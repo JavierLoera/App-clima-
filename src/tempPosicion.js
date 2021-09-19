46787aad15b37cf3c60ddd2053ba62d925b7b5c1
JavierLoera <78636781+JavierLoera@users.noreply.github.com>
2021-09-18 19:40:53 -0500

    Update tempPosicion.js

--- src/tempPosicion.js
+++ src/tempPosicion.js
@@ -1,7 +1,7 @@
-const tempPosicion = async (position) => {
+const tempPosicion = async (geolocation) => {
 	const API_KEY = "b189ddc5a38fca7f7e76afb8a2ea7919";
 	const res = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=${API_KEY}&lang=es`
+		`https://api.openweathermap.org/data/2.5/weather?lat=${geolocation.latitude}&lon=${geolocation.longitude}&APPID=${API_KEY}&lang=es`
 	);
 	const data = await res.json();
 	return data;
